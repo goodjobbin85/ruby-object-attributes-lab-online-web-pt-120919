f94dad192636d35664f5f6a04162bd342643adc2
thomas dubiel <td1729@hotmail.com>
2019-12-22 17:13:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,4 +19,19 @@ end
 
 
 class Person 
+  def name=(name) 
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def job=(job) 
+    @job = job 
+  end 
+  def job 
+    @job 
+  end 
+  
 end
